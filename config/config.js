db0fed751b564dc6a4171bf4f3036b029892adc6
Benyam Simayehu <50404694+Benyam-S@users.noreply.github.com>
2022-07-11 16:49:51 +0300

    environment variables modified

--- config/config.js
+++ config/config.js
@@ -4,13 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  port: process.env.PORT || 5000,
-  ip: process.env.HOST || "127.0.0.1",
-  allowedOrigins: [process.env.ALLOWED_ORIGINS.split(", ")],
+  port: process.env.NUXT_ENV_PORT || 5000,
+  ip: process.env.NUXT_ENV_HOST || "127.0.0.1",
+  allowedOrigins: [process.env.NUXT_ENV_ALLOWED_ORIGINS.split(", ")],
   newsAPIBaseURL: "https://newsapi.org/v2/top-headlines",
-  newsAPIKey: process.env.NEWS_API_KEY,
-  emailAddress: process.env.EMAIL_ADDRESS,
-  emailPassword: process.env.EMAIL_PASSWORD,
-  emailHost: process.env.EMAIL_HOST,
-  emailPort: process.env.EMAIL_PORT ?? 465,
+  newsAPIKey: process.env.NUXT_ENV_NEWS_API_KEY,
+  emailAddress: process.env.NUXT_ENV_EMAIL_ADDRESS,
+  emailPassword: process.env.NUXT_ENV_EMAIL_PASSWORD,
+  emailHost: process.env.NUXT_ENV_EMAIL_HOST,
+  emailPort: process.env.NUXT_ENV_EMAIL_PORT ?? 465,
 };
